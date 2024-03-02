abd7e1f2d7539c4056e150a59594c11864bce993
tulolo287 <iv1055@yandex.ru>
2024-03-03 02:05:05 +0300

    init

---
+++ core/src/asteroids/game/AsteroidsGame.java
@@ -0,0 +1,20 @@
+package asteroids.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class AsteroidsGame extends ApplicationAdapter {
+	
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+	}
+	
+	@Override
+	public void dispose () {
+	}
+}
